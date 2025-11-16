515aab033d0a224165fd0f3a47d37a85d8504eb2
jesselam00 <jesselam2000@gmail.com>
2025-11-15 18:23:42 -0600

    Add video upload functionality that stores videos into the AWS S3 bucket configured in .env

--- main.go
+++ main.go
@@ -18,13 +18,13 @@ type apiConfig struct {
 	db               database.Client
 	jwtSecret        string
 	platform         string
+	s3Client		 *s3.Client
 	filepathRoot     string
 	assetsRoot       string
 	s3Bucket         string
 	s3Region         string
 	s3CfDistribution string
 	port             string
-	s3Client		 *s3.Client
 }
 
 func main() {
@@ -80,23 +80,23 @@ func main() {
 		log.Fatal("PORT environment variable is not set")
 	}
 
-	awsCfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(s3Region))
+	awsCfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(s3Region))
 	if err != nil {
-		log.Fatal("Unable to load aws configuration")
+		log.Fatal(err)
 	}
-	s3Client := s3.NewFromConfig(awsCfg)
+	client := s3.NewFromConfig(awsCfg)
 
 	cfg := apiConfig{
 		db:               db,
 		jwtSecret:        jwtSecret,
 		platform:         platform,
+		s3Client: 		  client,
 		filepathRoot:     filepathRoot,
 		assetsRoot:       assetsRoot,
 		s3Bucket:         s3Bucket,
 		s3Region:         s3Region,
 		s3CfDistribution: s3CfDistribution,
 		port:             port,
-		s3Client: 		  s3Client,
 	}
 
 	err = cfg.ensureAssetsDir()
